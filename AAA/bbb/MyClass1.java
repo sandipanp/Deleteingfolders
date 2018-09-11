2e8986d2fcd4b3fef543067ddd8c61898ff586e0
Sandipan Pramanik <sandipan.mca@gmail.com>
2018-09-11 21:04:23 +0530

    First Commit

---
+++ AAA/bbb/MyClass1.java
@@ -0,0 +1,9 @@
+package AAA.bbb;
+
+public class MyClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In class 1");
+	}
+}
---
+++ AAA/ccc/MyClass4.java
@@ -0,0 +1,10 @@
+package AAA.ccc;
+
+public class MyClass4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In class 4");
+	}
+
+}
